88d45699e5547e42a3b4df4c195ef538d588e3b1
Edward Montilla <gmont153@mtroyal.ca>
2024-02-12 11:04:57 -0700

    Added is_hit function

--- MODEL.C
+++ MODEL.C
@@ -1,13 +1,22 @@
 #include "MODEL.H"
 
 void move_reticle(Reticle *reticle) {
-    reticle->x += reticle->dx;
-    reticle->y += reticle->dy;
-}
+  reticle->x += reticle->dx;
+  reticle->y += reticle->dy;
+};
 
 void move_mallard(Mallard *mallard) {
-    if (mallard->is_dead) return;
+  if (mallard->is_dead)
+    return;
 
-    mallard->x += mallard->dx;
-    mallard->y += mallard->dy;
-}
+  mallard->x += mallard->dx;
+  mallard->y += mallard->dy;
+};
+
+bool is_hit(Reticle *reticle, Mallard *mallard) {
+  bool hit = false;
+  if (reticle->x == mallard->x && reticle->y == mallard->y) {
+    hit = true;
+  }
+  return hit;
+};
