8cf3c7e0cf32517ad09c752c0f9f0e3a325c0837
Jaunie Williams <jauniew21@gmail.com>
2024-02-16 17:14:50 -0700

    specific render functions finished! and their documentation.

--- RENDERER.C
+++ RENDERER.C
@@ -16,3 +16,20 @@
 void render_reticle(const Reticle *ret, UINT16 *base) {
     plot_bitmap_16(base, ret->x, ret->y, reticle_bitmap, ret->height);
 }
+
+/*******************************************************************************
+ * FUNCTION NAME: render_mallard                                               *
+ *                                                                             *
+ * PURPOSE: Plots a raster graphic of a mallard.                               *   
+ *                                                                             *
+ * INPUT: *mallard = a Mallard object.                                         *
+ *                                                                             *
+ * OUTPUT: no return value from function, plots a mallard on the screen.       *
+ *                                                                             *
+ * ASSUMPTION: mallard and base have been instantiated, and                    *
+ *           the object's x and y are within bounds.                           *
+ *                                                                             *
+ *******************************************************************************/
+void render_mallard(const Mallard *mallard, UINT16 *base) {
+    plot_bitmap_16(base, mallard->x, mallard->y, mallard_bitmap, mallard->height);
+}
