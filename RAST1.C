1598d8265b55f6f5ea366e41e544749186e37265
Jaunie Williams <jauniew21@gmail.com>
2024-03-17 22:43:39 -0600

    used fonts

--- RAST1.C
+++ RAST1.C
@@ -18,6 +18,14 @@ int main()
     void *base = Physbase();
 
 	clear_screen(base, 0);
+    plot_text(base, 0, 0, font, 52);
+    plot_text(base, 8, 0, font, 41);
+    plot_text(base, 16, 0, font, 45);
+    plot_text(base, 24, 0, font, 37);
+    plot_text(base, 32, 0, font, 50);
+    plot_text(base, 40, 0, font, 26);
+    plot_text(base, 16, 10, font, 22);
+    plot_text(base, 24, 10, font, 16);
 	plot_bitmap_16(base, 320, 200, reticle_bitmap, 19);
 	plot_bitmap_16(base, 49, 49, mallard_bitmap, 14);
 	plot_bitmap_16(base, 591, 351, mallard_bitmap, 14);
