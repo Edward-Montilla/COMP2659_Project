d5c1558a3ca9102c01ef2782e55054706c7ea7f1
Edward Montilla <gmont153@mtroyal.ca>
2024-03-18 13:11:47 -0600

    added explosion key 'g', music is on the way

--- MUSIC.C
+++ MUSIC.C
@@ -1,3 +1,33 @@
 #include <osbind>
+#include "PSG.H"
+#include "MUSIC.H"
 
-int main() { return 0; }
+Notes opening[] = {
+	/* the opening theme of the game */
+
+};
+
+Notes laugh[] ={
+
+
+};
+
+void start_music(){
+
+
+
+};
+
+void update_music(UINT32 time_elapsed){
+
+
+};
+
+void start_dog_laugh(){
+
+
+};
+
+void update_dog_laugh(){
+
+};
